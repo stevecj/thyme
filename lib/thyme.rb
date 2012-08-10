2a15f76f0aa48e14467db4e44baf5e19b66f2139
Steve Jorgensen <stevej@renewfund.com>
2012-08-10 08:20:17 -0700

    Oops - a bit of code cleanup called for before proceeding.

--- lib/thyme.rb
+++ lib/thyme.rb
@@ -5,6 +5,15 @@ class Thyme
     @options = options
   end
 
+  def show_activity_time_console(renderer)
+    data = dates_in_week_of(today).map{|date|
+      { :date => date, :entries => [] }
+    }
+    renderer.call data
+  end
+
+  private
+
   def today
     @date_source ||= ( @options[:date_source] || Date.method(:today) )
     @date_source.call
@@ -14,12 +23,5 @@ class Thyme
     monday = date_in_week + 1 - date_in_week.cwday
     sunday = monday + 6
     (monday..sunday)
-  end
-
-  def show_activity_time_console(renderer)
-    data = dates_in_week_of(today).map{|date|
-      { :date => date, :entries => [] }
-    }
-    renderer.call data
   end
 end
